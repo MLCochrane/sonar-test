cda558a41e5acfdec6e000aa6279f77a83541e55
Luke <lukecochrane2@gmail.com>
2022-03-26 14:46:57 -0400

    Cleanup, adding status step

---
+++ scripts/get-status.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Lightly modified from https://www.apriorit.com/dev-blog/748-qa-integrating-quality-control-into-cicd
+# Just fetches the project status from the API and then returns an exit code to set the CircleCI job status
+sudo apt-get install jq -y
+PROJECTKEY="my:project"
+
+QGSTATUS=`curl -s -u admin:admin http://localhost:9000/api/qualitygates/project_status?projectKey=$PROJECTKEY | jq '.projectStatus.status' | tr -d '"'`
+
+if [ "$QGSTATUS" = "OK" ]
+then
+  echo "Status is OK"
+  exit 0
+else
+  echo "Status was not OK - review issues artifact for more details."
+  exit 1
+fi
