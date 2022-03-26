c7024a878cc9fd57309d8d51ac8b1ebee3906c38
Luke <lukecochrane2@gmail.com>
2022-03-26 19:53:16 -0400

    moving status check into server container

--- scripts/get-status.sh
+++ scripts/get-status.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-# Lightly modified from https://www.apriorit.com/dev-blog/748-qa-integrating-quality-control-into-cicd
-# Just fetches the project status from the API and then returns an exit code to set the CircleCI job status
+# Lightly modified from
+# https://www.apriorit.com/dev-blog/748-qa-integrating-quality-control-into-cicd.
+# Just fetches the project status from the API and then returns an exit code to
+# set the CircleCI job status
+
+
+# This will run INSIDE the server container and not as a normal job on the host
+# container since we can't expose arbitrary ports in CircleCI. That is why we
+# have apk and wget - they're already in the image and reduces the number of
+# extra steps we need to take.
+
 PROJECTKEY="my:project"
 
-QGSTATUS=`curl -s -u admin:admin http://localhost:9000/api/qualitygates/project_status?projectKey=$PROJECTKEY | jq '.projectStatus.status' | tr -d '"'`
+QGSTATUS=`wget -qO- http://admin:admin@localhost:9000/api/qualitygates/project_status?projectKey=$PROJECTKEY | jq '.projectStatus.status' | tr -d '"'`
 
-echo $QGSTATUS
+# We just want an exit code based on the project status - WARN vs. ERROR is less
+# a concern here.
 if [ "$QGSTATUS" = "OK" ]
 then
   echo "Status is OK"
